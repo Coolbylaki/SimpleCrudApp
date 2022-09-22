e7650e456157b297c523c18b663c4601506c5476
Lazar Stojanović <lakisd98@gmail.com>
2022-09-22 18:42:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true, }));
+app.use(express.json());
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+];
+
+app.get("/comments", (req, res) => {
+    res.render("comments/index", { comments })
+})
+
+app.get("/comments/new", (req, res) => {
+    res.render("comments/new")
+})
+
+app.post("/comments", (req, res) => {
+    const { username, comment } = req.body
+    comments.push({ username, comment })
+    res.send("IT WORKED!")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
